6dca0744fc6e3d0709c7de870b00984a39162ea1
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-01-13 16:27:40 +0100

    add : Database Class

---
+++ app/config/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database
+{
+    private string $host = 'localhost';
+    private string $db   = 'talent_hub_db';
+    private string $user = 'root';
+    private string $pass = '';
+    private ?PDO $conn = null;
+
+    public function connect(): PDO
+    {
+        if ($this->conn === null) {
+            $dsn = "mysql:host={$this->host};dbname={$this->db};";
+            $this->conn = new PDO($dsn, $this->user, $this->pass);
+
+        }
+        return $this->conn;
+    }
+}
