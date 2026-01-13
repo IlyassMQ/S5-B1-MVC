ec62fbf0907ce058ca412bea404f2495d4b1d82a
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-01-13 16:26:24 +0100

    add : Files l’arborescence MVC

---
+++ db.sql
@@ -0,0 +1,22 @@
+-- Active: 1768298840550@@127.0.0.1@3306
+
+USE talent_hub_db;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+
+    CONSTRAINT user_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
